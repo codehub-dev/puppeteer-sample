8d51ee84192b108a0560f8aacef53d1b1dd3d611
すー <aaaiiiuuu35@gmail.com>
2020-02-29 23:05:50 +0900

    mouse init

---
+++ src/mouse/mouse.ts
@@ -0,0 +1,17 @@
+import puppeteer from 'puppeteer';
+
+const mouse1 = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    defaultViewport: { width: 1920, height: 1080 },
+    slowMo: 100,
+  });
+  (async () => {
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/', { waitUntil: 'load' });
+  })().finally(async () => {
+    await browser.close();
+  });
+};
+mouse1();
