976178eed686011b08e4d7dc5ffc27972bb089c4
すー <aaaiiiuuu35@gmail.com>
2020-03-01 00:23:44 +0900

    mouse

--- src/mouse/mouse.ts
+++ src/mouse/mouse.ts
@@ -1,4 +1,6 @@
 import puppeteer from 'puppeteer';
+import { xType } from '@/puppeteer/xPath';
+import { mouseHelper } from '@/puppeteer/mouseHelper';
 
 const mouse1 = async () => {
   const browser = await puppeteer.launch({
@@ -10,6 +12,25 @@ const mouse1 = async () => {
   (async () => {
     const page = await browser.newPage();
     await page.goto('https://www.google.com/', { waitUntil: 'load' });
+
+    // @ts-ignore
+    await page.evaluate(mouseHelper);
+    await page.mouse.move(200, 200);
+
+    await xType(
+      page,
+      '//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input',
+      'R6SRS6R6SRS6R6SRS6R6SRS6'
+    );
+
+    await page.waitFor(1000);
+
+    await page.mouse.move(900, 450);
+    await page.mouse.click(900, 450);
+
+    await page.waitForNavigation();
+
+    await page.waitFor(10000);
   })().finally(async () => {
     await browser.close();
   });
