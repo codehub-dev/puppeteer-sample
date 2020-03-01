b647cbe60a9007dd54cc9469370404029f255d25
すー <aaaiiiuuu35@gmail.com>
2020-03-01 15:09:36 +0900

    Update click.ts

--- src/page/click.ts
+++ src/page/click.ts
@@ -7,20 +7,22 @@ const sample1 = async () => {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     defaultViewport: { width: 1920, height: 1080 },
     slowMo: 200,
-    devTools: true,
   });
   (async () => {
     const page = await browser.newPage();
     await page.goto('https://www.google.com/', { waitUntil: 'load' });
     await page.evaluate(mouseHelper);
 
+    // クリックでページ移動
     await Promise.all([
       page.click('#hptl > a:nth-child(1)', { button: 'left' }),
       page.waitForNavigation(),
     ]);
 
+    // 戻る
     await Promise.all([page.goBack(), page.waitForNavigation()]);
 
+    // ホバー
     await page.hover(
       '#tsf > div:nth-child(2) > div.A8SBwf > div.FPdoLc.tfB0Bf > center > input.gNO89b'
     );
