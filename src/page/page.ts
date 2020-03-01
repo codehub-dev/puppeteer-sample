40b2a35aebcc96addb62fd98a17ae7f9fa61d86d
すー <aaaiiiuuu35@gmail.com>
2020-03-01 14:49:22 +0900

    Update page.ts

---
+++ src/page/page.ts
@@ -0,0 +1,25 @@
+import puppeteer from 'puppeteer';
+
+const sample1 = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    defaultViewport: { width: 1920, height: 1080 },
+    slowMo: 100,
+  });
+  (async () => {
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/', { waitUntil: 'load' });
+
+    await page.once('load', () => console.log('Page loaded!'));
+
+    await page.on('console', msg => {
+      for (let i = 0; i < msg.args().length; ++i)
+        console.log(`${i}: ${msg.args()[i]}`);
+    });
+    await page.evaluate(() => console.log('hello', 5, { foo: 'bar' }));
+  })().finally(async () => {
+    await browser.close();
+  });
+};
+sample1();
